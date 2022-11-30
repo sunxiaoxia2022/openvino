76696fb548fd45ea4500e5b4114417abb816319d
sunxiaoxia2022 <xiaoxia.sun@intel.com>
2022-11-30 16:03:31 +0800

    fix clang-format issue

--- src/inference/src/threading/ie_istreams_executor.cpp
+++ src/inference/src/threading/ie_istreams_executor.cpp
@@ -449,12 +449,12 @@ IStreamsExecutor::Config IStreamsExecutor::Config::MakeDefaultMultiThreaded(cons
             UpdateHybridCustomThreads(streamExecutorConfig);
         }
         std::cout << "[ p_e_core_info ] streams (threads): " << streamExecutorConfig._streams << "("
-                       << streamExecutorConfig._threads_per_stream_big * streamExecutorConfig._big_core_streams +
-                              streamExecutorConfig._threads_per_stream_small * streamExecutorConfig._small_core_streams
-                       << ") -- PCore: " << streamExecutorConfig._big_core_streams << "("
-                       << streamExecutorConfig._threads_per_stream_big
-                       << ")  ECore: " << streamExecutorConfig._small_core_streams << "("
-                       << streamExecutorConfig._threads_per_stream_small << ")";
+                  << streamExecutorConfig._threads_per_stream_big * streamExecutorConfig._big_core_streams +
+                         streamExecutorConfig._threads_per_stream_small * streamExecutorConfig._small_core_streams
+                  << ") -- PCore: " << streamExecutorConfig._big_core_streams << "("
+                  << streamExecutorConfig._threads_per_stream_big
+                  << ")  ECore: " << streamExecutorConfig._small_core_streams << "("
+                  << streamExecutorConfig._threads_per_stream_small << ")";
     }
 #endif
     const auto hwCores =
